0c8d305ec164e47b10ab7ccc816dd8991981a7c6
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-09 00:54:07 -0500

    add: CRUD en app Producto

--- project/producto/urls.py
+++ project/producto/urls.py
@@ -4,6 +4,19 @@ app_name = "producto"
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('productocategoria/create/', views.productocategoria_create,
-         name="productocategoria_create")
+    path('productocategoria/create/',
+         views.productocategoria_create,
+         name="productocategoria_create"),
+    path('productocategoria/list/',
+         views.productocategoria_list,
+         name="productocategoria_list"),
+    path('productocategoria/detail/<int:pk>/',
+         views.productocategoria_detail,
+         name="productocategoria_detail"),
+    path('productocategoria/update/<int:pk>/',
+         views.productocategoria_update,
+         name="productocategoria_update"),
+    path('productocategoria/delete/<int:pk>/',
+         views.productocategoria_delete,
+         name="productocategoria_delete"),
 ]
--- project/producto/views.py
+++ project/producto/views.py
@@ -4,6 +4,10 @@ from . import models, forms
 
 
 def home(request):
+    return render(request, "producto/index.html")
+
+
+def productocategoria_list(request):
     consulta = request.GET.get("consulta", None)
     if consulta:
         print(consulta)
@@ -12,7 +16,7 @@ def home(request):
     else:
         query = models.ProductoCategoria.objects.all()
     context = {"productos": query}
-    return render(request, "producto/index.html", context)
+    return render(request, "producto/productocategoria_list.html", context)
 
 
 def productocategoria_create(request):
@@ -25,3 +29,28 @@ def productocategoria_create(request):
     else:
         form = forms.ProductoCategoriaForm()
     return render(request, "producto/productocategoria_create.html", context={"form": form})
+
+
+def productocategoria_detail(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    return render(request, "producto/productocategoria_detail.html", context={"producto": query})
+
+
+def productocategoria_update(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    if request.method == "POST":
+        form = forms.ProductoCategoriaForm(request.POST, instance=query)
+        if form.is_valid:
+            form.save()
+            return redirect("producto:productocategoria_list")
+    else:
+        form = forms.ProductoCategoriaForm(instance=query)
+    return render(request, "producto/productocategoria_update.html", context={"form": form})
+
+
+def productocategoria_delete(request, pk: int):
+    query = models.ProductoCategoria.objects.get(id=pk)
+    if request.method == "POST":
+        query.delete()
+        return redirect("producto:productocategoria_list")
+    return render(request, "producto/productocategoria_delete.html", context={"producto": query})
