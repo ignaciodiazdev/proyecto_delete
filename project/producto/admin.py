1f66d0e3af0d0a37db5afbc87de57e329ec2a832
ignaciodz <ignaciodiazbr@gmail.com>
2024-05-11 21:31:05 -0500

    feature(producto): productocategoria > admin

--- project/producto/admin.py
+++ project/producto/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register([models.ProductoCategoria])
+admin.site.site_title = "Productos"
+
+
+class ProductoCategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+    list_display_links = ("nombre",)
+
+
+admin.site.register(models.ProductoCategoria, ProductoCategoriaAdmin)
